a06b2c3428eaec3acbf39887b5809371bfab4e32
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2022-10-05 16:30:03 -0500

    adjusting search bar padding for mobile

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ export default function Home( { properties }, {tagSearch} ) {
           </div>
         <div>
           <div className="flex justify-center items-center">
-            <div className="flex w-fit pt-20 pb-5 align-middle justify-center items-center">
+            <div className="flex w-fit pt-40 pb-5 align-middle justify-center items-center">
               <Input
                 type="text"
                 className="block w-100px border-2 rounded border-stone-700"

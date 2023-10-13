c7295e27092a681597239a11bb5a2e6537b74893
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-12 23:20:36 -0400

    styles

--- Components/Modal.js
+++ Components/Modal.js
@@ -10,17 +10,17 @@ export default function Modal ({property} ) {
     
   return ( 
         <>
-          <div className="cursor-pointer px">
-          <img src={property.url} onClick={toggleModal} alt="image modal"/>
-          {modal && (
-            <div className="modal">
-                <div className="overlay" onClick={toggleModal}>
-                    <div className="modal-content">
-                        <img src={property.url} onClick={toggleModal} alt="image modal"/>
-                    </div>
-                </div>
-            </div>
-            )}        
+          <div className="cursor-pointer">
+            <img className="shadow-lg" src={property.url} onClick={toggleModal} alt="image modal"/>
+            {modal && (
+              <div className="modal">
+                  <div className="overlay" onClick={toggleModal}>
+                      <div className="modal-content">
+                          <img src={property.url} onClick={toggleModal} alt="image modal"/>
+                      </div>
+                  </div>
+              </div>
+              )}        
           </div>
         </>
   );
--- Components/Photocard.js
+++ Components/Photocard.js
@@ -3,8 +3,7 @@ import Modal from "./Modal";
 
 export default function Photocard({ property, searchTag, tagSearch, resetTagSearch, currentPage }) {
   return (
-    <div className="pb-3">
-      <div className="flex flex-col max-w-sm rounded bg-slate-100 shadow-lg mx-auto"> 
+      <div className="px-5 py-5 flex flex-col max-w-sm rounded bg-slate-100 shadow-lg mx-auto"> 
         <Modal property={property} />
         <div className="px-6 py-4 flex flex-col items-center justify-center h-full">
           <div className="text-center px-20 text-white bg-black text-xl rounded mb-2">
@@ -24,6 +23,5 @@ export default function Photocard({ property, searchTag, tagSearch, resetTagSear
           </div>
         </div>
       </div>
-    </div>
   );
 }

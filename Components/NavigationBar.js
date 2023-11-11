8d51565cbd6c233bcdec9f22b152a60d15322cf3
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-11-10 20:41:21 -0500

    nav

--- Components/NavigationBar.js
+++ Components/NavigationBar.js
@@ -14,7 +14,7 @@ export default function NavigationBar ({ parsedData, setCurrentPage, currentPage
       }
 
     return (
-        <>
+      <>
         <div className="w-full">
           <div className="flex justify-center items-center">
             <div className="flex flex-wrap px-6 mt-2 pt-1 pb-9 align-middle justify-center items-center">
@@ -35,10 +35,11 @@ export default function NavigationBar ({ parsedData, setCurrentPage, currentPage
             <ShuffleButton 
                 shufflePhotos={shufflePhotos}
                 setVisiblePhotos={setVisiblePhotos}
-              />
-              </div>
+            />
+            
+            </div>
           </div>
         </div>
-    </>
+      </>
     )
 }

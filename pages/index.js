700377aa23163fc4b9b3e52dda954555876be211
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2024-05-20 23:20:51 -0400

    white space

--- pages/index.js
+++ pages/index.js
@@ -26,74 +26,73 @@ export async function getServerSideProps() {
 //Home function returns html for Home page 
 export default function Home( { parsedData, setCurrentPage } ) {
 
-    const [data, setData] = useState(parsedData);
-    const [visiblePhotos, setVisiblePhotos] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
+  const [data, setData] = useState(parsedData);
+  const [visiblePhotos, setVisiblePhotos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const router = useRouter();
+  const router = useRouter();
 
-    useEffect(()=>{
-      setVisiblePhotos(parsedData);
-    },[parsedData])
+  useEffect(()=>{
+    setVisiblePhotos(parsedData);
+  },[parsedData])
 
-    // searchItems handles the searchbar by updating visiblePhotos
-    const searchItems = (searchValue) => {
-      setSearchTerm(searchValue)
-      if (searchValue !=="") {
-        const filteredData = data.filter((item) => {
-            console.log(item.tags);
-            return Object.values(item).join("").toLowerCase().includes(searchValue.toLowerCase())
-      })
-      setVisiblePhotos(filteredData);
-    }}
-
-    // handles updating visiblePhotos by clicking on a tag
-    const searchTag = (tagSearch) => {
-    console.log(tagSearch);
-    if (tagSearch !=="") {
+  // searchItems handles the searchbar by updating visiblePhotos
+  const searchItems = (searchValue) => {
+    setSearchTerm(searchValue)
+    if (searchValue !=="") {
       const filteredData = data.filter((item) => {
-        return Object.values(item.tags).join("").toLowerCase().includes(tagSearch.toString().toLowerCase())
-      })
-      setVisiblePhotos(filteredData);
-      console.log(filteredData)
-    }}
+          console.log(item.tags);
+          return Object.values(item).join("").toLowerCase().includes(searchValue.toLowerCase())
+    })
+    setVisiblePhotos(filteredData);
+  }}
 
-    // handles updating via search bar entry
-    const setValueFromInput = (value) => {
-      searchItems(value)
-    };
+  // handles updating visiblePhotos by clicking on a tag
+  const searchTag = (tagSearch) => {
+  console.log(tagSearch);
+  if (tagSearch !=="") {
+    const filteredData = data.filter((item) => {
+      return Object.values(item.tags).join("").toLowerCase().includes(tagSearch.toString().toLowerCase())
+    })
+    setVisiblePhotos(filteredData);
+    console.log(filteredData)
+  }}
 
-    // shuffles photos -- currently just a page refresh
-    const shufflePhotos = () => {
-      router.reload();
-    }
+  // handles updating via search bar entry
+  const setValueFromInput = (value) => {
+    searchItems(value)
+  };
 
-    return (
-      
-        <div className="background">
-            <Head>
-              <title>Bob Reuter Photography Archive</title>
-                <meta name="description" content="Bob Reuter Digital Archive" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </Head>
-            <Script src="https://kit.fontawesome.com/2fda98f1da.js" crossOrigin="anonymous" async></Script>
-            <div>
-              <Navbar />
-            </div>
-            <main>
-            <div className="photo-collection-external-container">
-              <SearchFilter 
-                visiblePhotos = {visiblePhotos}
-                setVisiblePhotos = {setVisiblePhotos}
-                shufflePhotos = {shufflePhotos}
-                searchTag = {searchTag}
-                parsedData = {parsedData}
-                setValueFromInput = {setValueFromInput}
-                searchTerm = { searchTerm }
-                setSearchTerm = { setSearchTerm }
-              />
-            </div>
-          </main>
-        </div>
-    )
+  // shuffles photos -- currently just a page refresh
+  const shufflePhotos = () => {
+    router.reload();
   }
+
+  return (
+      <div className="background">
+          <Head>
+            <title>Bob Reuter Photography Archive</title>
+              <meta name="description" content="Bob Reuter Digital Archive" />
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+          </Head>
+          <Script src="https://kit.fontawesome.com/2fda98f1da.js" crossOrigin="anonymous" async></Script>
+          <div>
+            <Navbar />
+          </div>
+          <main>
+          <div className="photo-collection-external-container">
+            <SearchFilter 
+              visiblePhotos = {visiblePhotos}
+              setVisiblePhotos = {setVisiblePhotos}
+              shufflePhotos = {shufflePhotos}
+              searchTag = {searchTag}
+              parsedData = {parsedData}
+              setValueFromInput = {setValueFromInput}
+              searchTerm = { searchTerm }
+              setSearchTerm = { setSearchTerm }
+            />
+          </div>
+        </main>
+      </div>
+  )
+}
